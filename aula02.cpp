83f39392372cd26c29bd62d5c5f5b5b376fb06b5
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-01 16:12:06 -0300

    Create aula02.cpp

---
+++ aula02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	float a, b, soma;
+	
+	cout << "Entre com dois numeros para somar: " << endl;
+	
+	cout << "Digite o primeiro numero: ";
+	cin >> a;
+	
+	cout << "Digite o segundo numero: ";
+	cin >> b;
+	
+	soma = a + b;
+	
+	cout << "O resultado da soma foi: " << soma << endl;
+	
+	
+	
+	return 0;
+	
+}
