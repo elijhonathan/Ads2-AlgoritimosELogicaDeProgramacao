667caba40659b780ae7ba3e1b264f94ad4ef0200
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-03 01:04:24 -0300

    Create aula02-Exercicio03.cpp

---
+++ aula02-Exercicio03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float temperaturaf, celcius;
+	
+	cout << "Digite uma temperatura em Fahrenheit (F) para converter para Celsius (C): ";
+	cin >> temperaturaf;
+	
+	celcius = (temperaturaf - 32) / 1.8;
+	
+	cout << "A temperatura " << temperaturaf << " F fica  " << celcius << " C";
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
