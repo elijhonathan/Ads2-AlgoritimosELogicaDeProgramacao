ea6fc152420c582064ca47c7b6eb9b6c82688981
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-07 23:39:07 -0300

    Create aula03-Exercicio01-AnosMesesDias.cpp

---
+++ aula03-Exercicio01-AnosMesesDias.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int entrada, idade_anos, resto_anos, idade_meses, dias;
+
+    cout << "Digite sua idade em dias: ";
+    cin >> entrada;
+
+    idade_anos = entrada / 365;
+
+    resto_anos = entrada % 365;
+
+    idade_meses = resto_anos / 30;
+
+    dias = resto_anos % 30;
+
+
+    
+    cout << idade_anos << " anos" << endl;
+    cout << idade_meses << " meses" << endl;
+    cout << dias << " dias";
+
+}
