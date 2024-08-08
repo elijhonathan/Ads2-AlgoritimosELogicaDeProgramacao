098922ae9f500e2e2e50f2c501a0bf434228615a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-07 23:34:27 -0300

    Create aula03-exemplopar.cpp

---
+++ aula03-exemplopar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int numero, res;
+
+	cout << "Entre com um numero inteiro: ";
+	cin >> numero;
+	
+	res = numero % 2;
+	
+	cout << "Resto: " << res << endl;
+	
+	
+	
+}
