20a7fc25de8c9d71b504d9a3bf983c5a60906649
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-11 12:47:53 -0300

    Update aula03-Exercicio02-NumeroNotas.cpp

--- aula03-Exercicio02-NumeroNotas.cpp
+++ aula03-Exercicio02-NumeroNotas.cpp
@@ -11,23 +11,18 @@ int main()
     cin >> entrada;
 
     cem = entrada / 100;
-
     resto_cem = entrada % 100;
 
     cinquenta = resto_cem / 50;
-
     resto_cinquenta = resto_cem % 50;
 
     dez = resto_cinquenta / 10;
-
     resto_dez = resto_cinquenta % 10;
 
     cinco = resto_dez / 5;
-
     resto_cinco = resto_dez % 5;
 
     um = resto_cinco / 1;
-
 
     cout << cem << " nota de cem" << endl;
     cout << cinquenta << " nota de cinquenta" << endl;
