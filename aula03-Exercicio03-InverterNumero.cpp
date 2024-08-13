2db2b2874dcedbaecd43c1804b9ae26b06a1b057
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-13 10:08:30 -0300

    Update aula03-Exercicio03-InverterNumero.cpp

--- aula03-Exercicio03-InverterNumero.cpp
+++ aula03-Exercicio03-InverterNumero.cpp
@@ -11,15 +11,12 @@ cout << "Digite um valor numerico com tres unidades: " << endl;
 cin >> entrada;
 
 numero1 = entrada / 100;
-
 resto_um = entrada % 100;
 
 numero2 = resto_um / 10;
-
 resto_dois = resto_um % 10;
 
 numero3 = resto_dois;
 
 cout << numero3 << numero2 << numero1;
-    
 }
