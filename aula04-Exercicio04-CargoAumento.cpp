2c13cd79b056800127e4d8e2521cceca65d9c3b5
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-15 00:08:22 -0300

    Create aula04-Exercicio04-CargoAumento.cpp

---
+++ aula04-Exercicio04-CargoAumento.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float codigo, salario, gerente, engenheiro, tecnico;
+
+    cout << "Digite o codigo do seu cargo: ";
+    cin >> codigo;
+
+    cout << "Digite o seu salario: ";
+    cin >> salario;
+
+    if (codigo == 100 )
+    {
+        gerente = salario + (salario * 0,10);
+        cout << "O gerente tem um aumento de 10%, seu novo salario sera: R$ ";
+        cout << gerente;
+
+    }
+
+    else if (codigo == 101)
+    {
+        engenheiro = salario + (salario * 0,20);
+        cout << "O engenheiro tem um aumento de 20%, seu novo salario sera: R$ ";
+        cout << engenheiro;
+
+    }
+
+    else if (codigo == 102)
+    {
+        tecnico = salario + (salario * 0,30);
+        cout << "O tecnico tem um aumento de 30%, seu novo salario sera: R$ ";
+        cout << tecnico;
+
+    }
+
+    else
+    {
+        cout << "Erro, tente novamente!";
+    }
+
+
+
+}
