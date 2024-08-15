2f46e472ea16f8dc3032e9688ec786bb29dc501a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-15 00:02:57 -0300

    Create aula04-Exemplo06ParImpar.cpp

---
+++ aula04-Exemplo06ParImpar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    int num, resto;
+    
+    cout << "Entre com um numero inteiro: " << endl;
+    cin >> num;
+    
+    if (num % 2 == 0) {
+    	cout << "Par" << endl;
+	}
+	else {
+		cout << "Impar" << endl;
+	}
+	
+}
