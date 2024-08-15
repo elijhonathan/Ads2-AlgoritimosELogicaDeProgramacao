00cc2f2c313f15d2da0612214b7cf2a38685c3d3
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-15 00:03:51 -0300

    Create aula04-Exemplo07PositivoNegativoZero.cpp

---
+++ aula04-Exemplo07PositivoNegativoZero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    float num;
+    
+    num = -3;
+    
+    if (num > 0) {
+    	cout << "Positivo" << endl;
+	}
+	
+	if (num < 0) {
+		cout << "Negativo" << endl;
+	}
+	
+	if (num == 0) {
+		cout << "Zero" << endl;
+	}
+	
+}
