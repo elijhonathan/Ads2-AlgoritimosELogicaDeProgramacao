89360d1677682fde83eeba909524c5ec5267224b
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-14 23:49:27 -0300

    Create aula04-Exemplo01-Relacionas.cpp

---
+++ aula04-Exemplo01-Relacionas.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    int x, y;
+    
+    x = 10;
+    y = 5;
+    
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl << endl;
+    
+    // igualdade
+	cout << "x == 10 ? " << (x == 10) << endl;
+	cout << "x == y ? " << (x == y) << endl;
+	
+	
+	// diferen�a
+	cout << "x != 10 ? " << (x != 10) << endl;
+	cout << "x != y ? " << (x != y) << endl;
+
+	
+	// maior
+	cout << "x >= y ? " << (x >= y) << endl;
+
+	// menor
+	cout << "x <= y ? " << (x <= y) << endl;
+	
+}
