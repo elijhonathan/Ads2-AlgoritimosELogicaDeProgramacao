f9b3b8d09ff5acecba96a576ce42f6ee7a112f51
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-15 00:04:53 -0300

    Create aula04-Exercicio01-MediaIfElse.cpp

---
+++ aula04-Exercicio01-MediaIfElse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int media;
+
+    cout << "Digite a sua media: ";
+    cin >> media;
+
+    if (media >= 70)
+    {
+        cout << "Voce foi aprovado!";
+
+    }
+
+    else if(media < 70)
+    {
+        cout << "Voce foi reprovado!";
+    }
+
+
+
+
+
+}
