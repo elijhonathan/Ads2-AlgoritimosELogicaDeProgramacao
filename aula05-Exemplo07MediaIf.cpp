1f478c0ec9a7752b580e810e27add5f5d2cde730
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-24 14:36:19 -0300

    Create aula05-Exemplo07MediaIf.cpp

---
+++ aula05-Exemplo07MediaIf.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+Fa�a um programa que leia a m�dia de um aluno e mostre a sua 
+situa��o. Caso a m�dia seja maior ou igual a 70, o programa 
+deve emitir a mensagem �Aprovado�. Caso contr�rio, se a m�dia
+for menor do que 40, a mensagem a ser exibida � �Reprovado�; 
+sen�o, deve ser exibida a mensagem �Exame Final�.
+*/
+
+int main() {
+	
+	float media;
+	
+	cout << "Entre com a media do aluno: " << endl;
+	cin >> media;
+	
+	if (media >= 70) {
+		cout << "Aprovado" << endl;	
+	}
+	else {
+		
+		if (media < 40) {
+			cout << "Reprovado" << endl;
+		}
+		else {
+			cout << "Exame Final" << endl;
+		}		
+	}
+		
+}
