62e576f6874679688873c5cdf993e41e453675af
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-24 14:35:48 -0300

    Create Aula05-Exercicio06DiferenteMaior.cpp

---
+++ Aula05-Exercicio06DiferenteMaior.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/* 
+Fa�a um programa que leia dois n�meros inteiros e mostre uma
+mensagem informando se eles s�o iguais. 
+Se eles forem diferentes, mostre o maior n�mero.
+*/ 
+
+int main() {
+
+	int num1, num2;
+
+	cout << "Entre com dois numeros: " << endl;
+	cin >> num1 >> num2;
+	
+	if (num1 == num2) {  
+		cout << "Os numeros sao iguais" << endl;
+	}	
+	else {
+		if (num1 > num2) {
+			cout << "Maior: " << num1 << endl;
+		}
+		else {
+			cout << "Maior: " << num2 << endl;
+		}
+	}	
+}
