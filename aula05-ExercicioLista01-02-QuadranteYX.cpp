8846d2d237360928cbfcdf964a3a1f6856c71ea5
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-25 23:02:18 -0300

    Update aula05-ExercicioLista01-02-QuadranteYX.cpp

--- aula05-ExercicioLista01-02-QuadranteYX.cpp
+++ aula05-ExercicioLista01-02-QuadranteYX.cpp
@@ -10,19 +10,38 @@ int main() {
     cout << "Digite a coordenada Y: ";
     cin >> y;
     
-    if (x > 0 && y > 0) {
+    if (x > 0 && y > 0) 
+    {
         cout << "O ponto está no primeiro quadrante." << endl;
-    } else if (x < 0 && y > 0) {
+    }
+    
+    else if (x < 0 && y > 0) 
+    {
         cout << "O ponto está no segundo quadrante." << endl;
-    } else if (x < 0 && y < 0) {
+    }
+    
+    else if (x < 0 && y < 0)
+    {
         cout << "O ponto está no terceiro quadrante." << endl;
-    } else if (x > 0 && y < 0) {
+    }
+    
+    else if (x > 0 && y < 0) 
+    {
         cout << "O ponto está no quarto quadrante." << endl;
-    } else if (x == 0 && y == 0) {
+    } 
+    
+    else if (x == 0 && y == 0) 
+    {
         cout << "O ponto está na origem." << endl;
-    } else if (x == 0) {
+    } 
+    
+    else if (x == 0) 
+    {
         cout << "O ponto está sobre o eixo Y." << endl;
-    } else if (y == 0) {
+    } 
+    
+    else if (y == 0) 
+    {
         cout << "O ponto está sobre o eixo X." << endl;
     }
 
