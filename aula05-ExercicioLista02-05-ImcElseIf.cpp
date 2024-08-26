495b9788f53704dd6d6ac6b3841a74c4c8802f54
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-25 23:00:12 -0300

    Create aula05-ExercicioLista02-05-ImcElseIf.cpp

---
+++ aula05-ExercicioLista02-05-ImcElseIf.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+float imc, peso, altura;
+
+cout << "Digite a sua altura em Metros: ";
+cin >> altura;
+
+cout << "Digite a sua peso em kg: ";
+cin >> peso;
+
+imc = peso / (altura * altura);
+
+
+if(imc < 18.5)
+{
+    cout << "O seu IMC eh: " << imc << endl;
+    cout << "Voce esta abaixo do peso";
+}
+
+else if(imc >= 18.5 && imc < 25)
+{
+    cout << "O seu IMC eh: " << imc << endl;
+    cout << "Voce esta com o peso ideal";
+}
+
+else if(imc >= 25 && imc < 30)
+{
+    cout << "O seu IMC eh: " << imc << endl;
+    cout << "Voce esta com sobrepeso";
+}
+
+else if(imc > 30)
+{
+    cout << "O seu IMC eh: " << imc << endl;
+    cout << "Voce esta com obesidade";
+}
+
+else
+{
+    cout << "Voce digitou um valor invalido";
+}
+
+}
