99bb959f776e2b7df9d2f0d181be96c564ff39dc
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-25 22:52:31 -0300

    Create aula05-Exercicio01-Lista02-FrequenciaReprovado.cpp

---
+++ aula05-Exercicio01-Lista02-FrequenciaReprovado.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int frequencia, media;
+
+    cout << "Digite a frequencia: ";
+    cin >> frequencia;
+
+
+    if(frequencia < 75)
+    {
+        cout << "Reprovado por falta";
+    }
+    else
+    {
+        cout << "Digite sua media: ";
+        cin >> media;
+
+        if(media >= 70)
+        {
+            cout << "Aprovado";
+        }
+
+        else if(media < 40)
+        {
+            cout << "Reprovado";
+        }
+
+        else if(media > 40 && media < 70)
+        {
+            cout << "Exame final";
+        }
+    }
+
+
+
+
+
+
+
+}
