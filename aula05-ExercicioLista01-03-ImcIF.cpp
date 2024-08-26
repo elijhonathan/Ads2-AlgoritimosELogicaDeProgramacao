1fbd5060cdb5a0d060d9718b723bfd37c7bd15ed
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-25 22:55:03 -0300

    Create aula05-ExercicioLista01-03-ImcIF.cpp

---
+++ aula05-ExercicioLista01-03-ImcIF.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+float imc, peso, altura;
+
+cout << "Digite a sua altura em Metros: ";
+cin >> altura;
+
+cout << "Digite a sua peso em kg: ";
+cin >> peso;
+
+imc = peso / (altura * altura);
+
+
+if(imc < 18.5)
+{
+    cout << "O seu IMC eh: " << imc << endl;
+    cout << "Voce esta abaixo do peso";
+}
+
+if(imc >= 18.5 && imc < 25)
+{
+    cout << "O seu IMC eh: " << imc << endl;
+    cout << "Voce esta com o peso ideal";
+}
+
+if(imc >= 25 && imc < 30)
+{
+    cout << "O seu IMC eh: " << imc << endl;
+    cout << "Voce esta com sobrepeso";
+}
+
+if(imc > 30)
+{
+    cout << "O seu IMC eh: " << imc << endl;
+    cout << "Voce esta com obesidade";
+}
+
+}
