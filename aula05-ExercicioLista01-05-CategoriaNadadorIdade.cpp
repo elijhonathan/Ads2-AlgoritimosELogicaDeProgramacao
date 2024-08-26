f1ddc9bacdcf7cb318f26207baf4a752fa78934a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-08-25 22:56:31 -0300

    Create aula05-ExercicioLista01-05-CategoriaNadadorIdade.cpp

---
+++ aula05-ExercicioLista01-05-CategoriaNadadorIdade.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+    int idade;
+
+    cout << "Digite a idade do nadador: ";
+    cin >> idade;
+
+    if(idade < 5 && idade > 0)
+    {
+        cout << "Idade abaixo da minina aceitavel";
+    }
+
+    else if(idade >= 5 && idade <= 7)
+    {
+        cout << "O nadador esta na categoria Infantil A";
+    }
+
+    else if(idade > 7 && idade <= 10)
+    {
+        cout << "O nadador esta na categoria Infantil B";
+    }
+
+    else if(idade > 10 && idade <= 13)
+    {
+        cout << "O nadador esta na categoria Juvenil A";
+    }
+
+    else if(idade > 13 && idade <=17)
+    {
+        cout << "O nadador esta na categoria Juvenil B";
+    }
+
+    else if(idade > 17)
+    {
+        cout << "O nadador esta na categoria Adulto";
+    }
+
+    else
+    {
+        cout << "Idade digitada invalida";
+    }
+
+
+}
