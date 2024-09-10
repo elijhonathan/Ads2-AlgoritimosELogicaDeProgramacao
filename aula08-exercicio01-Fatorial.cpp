f78fc76c3ee4f658a30912705b2c54b79e5ad257
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-09-10 14:07:18 -0300

    Create aula08-exercicio01-Fatorial.cpp

---
+++ aula08-exercicio01-Fatorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero, somatorio, contador;
+    
+    cout << "Digite um numero para ver o fatorial ";
+    cin >> numero;
+    
+    
+    somatorio = 1;
+    contador = 1;
+    
+    while(contador <= numero)
+    {
+        somatorio = somatorio * contador;
+        
+        contador = contador + 1;
+        
+        
+    }
+    
+    cout << "O numero " << numero << " tem o fatorial " << somatorio;
+    
+}
