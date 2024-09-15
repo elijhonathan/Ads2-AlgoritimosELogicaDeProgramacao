abcdde96c6d282ff9518ea157f2ea0cda02247bd
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-09-14 22:04:51 -0300

    Create aula08-Exemplo03NumerosDeAAB.cpp

---
+++ aula08-Exemplo03NumerosDeAAB.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/* 
+Fa�a um programa que mostre os numeros inteiros de A ate B 
+(incluindo A e incluindo B).
+*/
+
+int main() {
+	
+	int i, n, a, b;
+	
+	cout << "Entre com o valor de A: ";
+	cin >> a;
+	
+	cout << "Entre com o valor de B: ";
+	cin >> b;
+	
+	i = a;
+	n = b;
+	while (i <= n) {
+		// imprimindo os n�meros um ao lado do outro
+		cout << i << " ";
+		i = i + 1;
+	}
+	
+	return 0;
+			
+}
