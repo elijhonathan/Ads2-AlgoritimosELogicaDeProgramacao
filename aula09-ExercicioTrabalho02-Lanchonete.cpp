63ca7274c7b19cba20efbdbe7fa0022c9fcb9b1b
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-09-15 02:14:33 -0300

    Create aula09-ExercicioTrabalho02-Lanchonete.cpp

---
+++ aula09-ExercicioTrabalho02-Lanchonete.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float codigo, total, hotdog, coxinha, pastel, refrigerante;
+
+    cout << "Digite o codigo do lanche: ";
+    cin >> codigo;
+
+    cout << "Digite a quantidade de lanches:  ";
+    cin >> total;
+
+    if (codigo == 100)
+    {
+        hotdog = total * 5.0;
+        cout << "O valor de " << total << " cachorro quente eh R$ ";
+        cout << hotdog;
+    }
+
+    else if (codigo == 101)
+    {
+        coxinha = total * 6.0;
+        cout << "O valor de " << total << " coxinha eh R$ ";
+        cout << coxinha;
+    }
+
+    else if (codigo == 102)
+    {
+        pastel = total * 7.0;
+        cout << "O valor de " << total << " pastel eh R$ ";
+        cout << pastel;
+    }
+
+    else if (codigo == 103)
+    {
+        refrigerante = total * 3.0;
+        cout << "O valor de " << total << " refrigerante eh R$ ";
+        cout << refrigerante;
+    }
+
+    else
+    {
+        cout << "Erro, tente novamente!";
+    }
+}
