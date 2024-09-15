d0a2355c36016148d7bc0e91833247f93b392b41
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-09-15 01:42:07 -0300

    Create aula09-Exercicio01-SomaNumerosPares.cpp

---
+++ aula09-Exercicio01-SomaNumerosPares.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int numeros, pares, soma, contador;
+
+    cout << "Digite o numero de entradas: ";
+    cin >> numeros;
+
+    contador = 1;
+    soma = 0;
+
+
+
+    while(contador <= numeros)
+    {
+        cout << "Digite o numero: ";
+        cin >> pares;
+
+        if(pares % 2 == 0)
+        {
+            soma = soma + pares;
+
+        }
+
+        contador++;
+
+    }
+
+    cout << "A soma dos numeros pares eh: " << soma;
+
+
+}
