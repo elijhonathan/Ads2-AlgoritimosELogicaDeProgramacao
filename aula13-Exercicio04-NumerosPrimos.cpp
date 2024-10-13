74bf437c4c7b93493b134f5c1d655139d377a626
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2024-10-13 04:45:42 -0300

    Create aula13-Exercicio04-NumerosPrimos.cpp

---
+++ aula13-Exercicio04-NumerosPrimos.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int numeroDePrimos, contador, i, primeiroNumeroPrimo;
+
+    contador = 0;
+    primeiroNumeroPrimo = 2;
+
+    cout <<"Digite quantos numeros primos voce quer exibir: " << endl;
+    cin >> numeroDePrimos;
+
+    while (contador < numeroDePrimos)
+    {
+        int divisor;
+        divisor = 0;
+
+        for (i = 2; i * i <= primeiroNumeroPrimo; i++)
+        {
+            if (primeiroNumeroPrimo % i == 0)
+            {
+                divisor++;
+            }
+        }
+
+        if (divisor == 0)
+        {
+            cout << primeiroNumeroPrimo << " ";
+            contador++;
+        }
+
+        primeiroNumeroPrimo++;
+
+    }
+
+  
+}
